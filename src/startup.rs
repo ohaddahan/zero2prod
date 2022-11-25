42e276ee26b35a475d163ad077a855297b7f5a40
ohaddahan <ohaddahan@gmail.com>
2022-11-25 18:35:21 +0200

    chapter 4 now

---
+++ src/startup.rs
@@ -0,0 +1,23 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::{Pool, Postgres};
+use std::net::TcpListener;
+
+pub fn run(
+    listener: TcpListener,
+    connection_pool: Pool<Postgres>,
+) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let db_pool = web::Data::new(connection_pool);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
