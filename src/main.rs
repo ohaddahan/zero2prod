5aa12a152e2e475c2d68c762a557ecbe0a83bc87
ohaddahan <ohaddahan@gmail.com>
2022-12-09 20:25:13 +0200

    yay

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
+use std::fmt::{Debug, Display};
+use tokio::task::JoinError;
 use zero2prod::configuration::get_configuration;
+use zero2prod::issue_delivery_worker::run_worker_until_stopped;
 use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                            error.cause_chain = ?e,
+                            error.message = %e,
+                            "{} failed",
+                            task_name
+            )
+        }
+        Err(e) => {
+            tracing::error!(
+            error.cause_chain = ?e, error.message = %e,
+            "{}' task failed to complete", task_name
+            )
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = Application::build(configuration.clone())
+        .await?
+        .run_until_stopped();
+    let application_task = tokio::spawn(application);
+    let worker = run_worker_until_stopped(configuration);
+    let worker_task = tokio::spawn(worker);
+    tokio::select! {
+       o = application_task => report_exit("API", o),
+        o = worker_task => report_exit("Background worker", o),
+    };
     Ok(())
 }
